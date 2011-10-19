cd9b3a082bb7d221138b1e6d0b2706baa7424d6f
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-10-19 10:12:40 -0600

    setup ssh configuration

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+gem "capistrano", :require => nil
+gem "highline", :require => nil
+gem "railsless-deploy", :require => nil
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,59 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'rubygems'
+require 'highline'
 
-set :scm, :subversion
+default_run_options[:pty] = true
+
+set :application, "capibuntu"
+set :repository,  "git@github.com:jalvarezsamayoa/capibuntu.git"
+
+set :user, "ubuntu"
+set :password, "ubuntu"
+
+set :deploy_to, "/home/#{user}"
+
+set :local_home, "/home/javier"
+set :remote_home, "/home/#{user}"
+
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :server_host, "168.168.2.6"
+
+role :web, "bfvm1"                          # Your HTTP server, Apache/etc
+role :app, server_host                          # This may be the same as your `Web` server
+role :db,  server_host, :primary => true # This is where Rails migrations will run
+
+namespace :capibuntu do
+
+  task :uname, :roles =>:web do
+    run "uname -a"
+  end
+
+  namespace :server do
+    desc "Run system updates"
+    task :update, :roles => :web, :except => { :no_release => true } do
+      sudo "apt-get update"
+      sudo "apt-get upgrade -y"
+    end
+  end
+  
+  namespace :setup do    
+
+    desc "Configure ssh connection without login"
+    task :ssh, :roles => :web, :except => { :no_release => true } do
+      public_key = File.open("#{local_home}/.ssh/id_rsa.pub","r")     
+      run "cd #{remote_home} && mkdir -p .ssh"
+      put public_key.read, "#{remote_home}/.ssh/authorized_keys"
+      put public_key.read, "#{remote_home}/.ssh/authorized_keys2"      
+      run "chmod 700 #{remote_home}/.ssh"
+      run "chmod 640 #{remote_home}/.ssh/authorized_keys2"      
+    end
+ 
+    
+  end
+end
+
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
